1fd43c44afcb926f3a130842d82276bf12fd356b
ap24110011075 <varshithayerra02@gmail.com>
2026-06-16 11:41:17 +0530

    Find lowest element in an array

--- DSA/Arrays/find_lowest_value.py
+++ DSA/Arrays/find_lowest_value.py
@@ -1,3 +1,8 @@
+# Problem: Find the minimum element in an array
+# Approach: Traverse the array and keep track of the smallest value seen so far.
+# Time Complexity: O(n)
+# Space Complexity: O(1) where do i need to add it 
+
 arr = [7,4,3,9,2,8]
 min_value = arr[0]
 
